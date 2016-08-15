959067cdeb909ff91572cf8adb251ad9755863b7
Sergey Shvager <sergey.shvager@home24.de>
2016-08-15 16:20:51 +0200

    fixed mixed content

--- app/reducers/products.js
+++ app/reducers/products.js
@@ -1,7 +1,7 @@
 const defaultState = [
         {
             sku: 'SKU-1',
-            img: 'http://cdn.home24.net/images/media/catalog/product/250x250/png/s/e/sessel-houston-i-webstoff-chrom-stoff-milan-gelb-3936076.jpg',
+            img: 'https://cdn.home24.net/images/media/catalog/product/250x250/png/s/e/sessel-houston-i-webstoff-chrom-stoff-milan-gelb-3936076.jpg',
             title: 'Sessel Houston I',
             author: 'jill111',
             amount: {
@@ -11,7 +11,7 @@ const defaultState = [
         },
         {
             sku: 'SKU-2',
-            img: 'http://cdn.home24.net/images/media/catalog/product/250x250/png/e/c/ecksofa-grapefield-webstoff-longchair-ottomane-davorstehend-rechts-meeresgruen-3177209.jpg',
+            img: 'https://cdn.home24.net/images/media/catalog/product/250x250/png/e/c/ecksofa-grapefield-webstoff-longchair-ottomane-davorstehend-rechts-meeresgruen-3177209.jpg',
             title: 'Ecksofa Grapefield',
             author: 'pashminu',
             amount: {
@@ -22,7 +22,7 @@ const defaultState = [
         {
 
             sku: 'SKU-3',
-            img: 'http://cdn.home24.net/images/media/catalog/product/250x250/png/t/i/tischleuchte-tripod-metall-eisen-1-flammig-740822.jpg',
+            img: 'https://cdn.home24.net/images/media/catalog/product/250x250/png/t/i/tischleuchte-tripod-metall-eisen-1-flammig-740822.jpg',
             title: 'Tischleuchte TRIPOD',
             author: 'Danson67',
             amount: {
@@ -33,7 +33,7 @@ const defaultState = [
         {
 
             sku: 'SKU-6',
-            img: 'http://cdn.home24.net/images/media/catalog/product/250x250/png/a/r/armlehnenstuhl-nicholas-kunstleder-hellgrau-1068902.jpg',
+            img: 'https://cdn.home24.net/images/media/catalog/product/250x250/png/a/r/armlehnenstuhl-nicholas-kunstleder-hellgrau-1068902.jpg',
             title: 'Armlehnenstuhl Nicholas I ',
             author: 'fancycravel',
             amount: {
@@ -44,7 +44,7 @@ const defaultState = [
         {
 
             sku: 'SKU-7',
-            img: ' http://cdn.home24.net/images/media/catalog/product/250x250/png/f/i/figur-woody-bumble-eiche-eiche-natur-3132513.jpg',
+            img: ' https://cdn.home24.net/images/media/catalog/product/250x250/png/f/i/figur-woody-bumble-eiche-eiche-natur-3132513.jpg',
             title: 'Figur Woody Bumble',
             author: 'jill111',
             amount: {
@@ -55,7 +55,7 @@ const defaultState = [
         {
 
             sku: 'SKU-8',
-            img: 'http://cdn.home24.net/images/media/catalog/product/250x250/png/i/n/indexliving-3603733.jpg',
+            img: 'https://cdn.home24.net/images/media/catalog/product/250x250/png/i/n/indexliving-3603733.jpg',
             title: 'Armlehnenstuhl Le Gaillard',
             author: 'BkrmadtyaKarki',
             amount: {
