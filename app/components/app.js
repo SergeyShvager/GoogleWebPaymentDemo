bfa0fcaa0825c2a2b3aec1c5b3f172a1ee052fb1
Sergey Shvager <sergey.shvager@home24.de>
2016-08-15 14:17:19 +0200

    added price

--- app/components/app.js
+++ app/components/app.js
@@ -3,8 +3,6 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/RaisedButton';
 import NotificationWidget from './../containers/notification-widget';
-import AuthView from './user/auth';
-import UserPreloader from './user/preloader';
 import { Link, hashHistory } from 'react-router';
 import ProductList from './product-list';
 import map from 'lodash/map';
--- app/components/product.js
+++ app/components/product.js
@@ -24,9 +24,10 @@ const Product = React.createClass({
         return (
             <GridTile
                 key={product.img}
+                titleBackground="rgba(0, 0, 0, 0.6)"
                 title={product.title}
-                subtitle={<span>by <b>{product.author}</b></span>}
-                actionIcon={<IconButton onClick={this.onClick}><AddToCartIcon color="orange" /></IconButton>}
+                subtitle={<span>{product.amount.value} <b>{product.amount.currency}</b></span>}
+                actionIcon={<IconButton onClick={this.onClick}><AddToCartIcon color="orange" /><b>Buy</b></IconButton>}
             >
                 <img src={`http://www.material-ui.com/${product.img}`} />
             </GridTile>
