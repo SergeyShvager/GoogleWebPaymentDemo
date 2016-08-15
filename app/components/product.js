ebb8ff2b11e4bfaa131bec66d695911e05fb4249
Sergey Shvager <sergey.shvager@home24.de>
2016-08-15 13:42:14 +0200

    stable version

--- app/components/product.js
+++ app/components/product.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {GridList, Gridproduct} from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import AddToCartIcon from 'material-ui/svg-icons/action/add-shopping-cart';
+
 const Product = React.createClass({
     propTypes: {
         onBuyClick: React.PropTypes.func.isRequired,
@@ -11,14 +12,14 @@ const Product = React.createClass({
     render() {
         const { product } = this.props;
         return (
-            <Gridproduct
+            <GridTile
                 key={product.img}
                 title={product.title}
                 subtitle={<span>by <b>{product.author}</b></span>}
                 actionIcon={<IconButton onClick={this.onClick}><AddToCartIcon color="orange" /></IconButton>}
             >
                 <img src={`http://www.material-ui.com/${product.img}`} />
-            </Gridproduct>
+            </GridTile>
         );
     }
 });
